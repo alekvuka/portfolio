144c7c8945b8cbb194d68bd0c1d16e6b29e95eb3
Aleksandar Vukasinovic <alekvuka@gmail.com>
2023-06-04 17:02:49 +0200

    centered header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,29 +5,17 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function Header() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+    
+    <Navbar bg="light" expand="lg" className="justify-content-center">
+          <Nav bg="light" expand="lg" className="justify-content-center">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#link">Examples of My Work</Nav.Link>
+            <Nav.Link href="#link">Projects I Contribnuted To</Nav.Link>
+            <Nav.Link href="#link">My Favorite Books</Nav.Link>
+            <Nav.Link href="#link">Contact</Nav.Link>
           </Nav>
-        </Navbar.Collapse>
-      </Container>
     </Navbar>
+    
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav'
-import Dropdown from 'react-bootstrap/Dropdown';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Container, Image } from 'react-bootstrap';
 import Header from '../components/Header';
 
 // Home component
@@ -10,148 +7,13 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <h1> </h1>
-      <ul>
-        <h5>Examples of my work:</h5>
-          <ul>
-          <li>
-          <Link to="/mywork">Stories</Link>
-          </li>
-          <li>
-          <Link to="/mywork">Epics</Link>
-          </li>
-          <li>
-          <Link to="/mywork">Product Roadmaps</Link>
-          </li>
-          <li>
-          <Link to="/mywork">UI/UX </Link>
-          </li>
-          </ul>
-        
-        
-          <h5>Projects I contributed to:</h5>
-          <ul>
-          <li>
-          <Link to="/mywork">Avalanche Insights</Link>
-          </li>
-          <li>
-          <Link to="/mywork">Emozia</Link>
-          </li>
-          <li>
-          <Link to="/mywork">Together.farm</Link>
-          </li>
-          <li>
-          <Link to="/mywork">MapMyStress</Link>
-          </li>
-          <li>
-          <Link to="/mywork">MapMyWork </Link>
-          </li>
-          <li>
-          <Link to="/mywork">Coronavirus Updates</Link>
-          </li>
-          <li>
-          <Link to="/mywork">NYC Traffic Cams</Link>
-          </li>
-          </ul>
-        
-          <h5>Favorie Books:</h5>
-          <ul>
-          <li>
-          <Link to="/mywork">The Startup Owners Manual</Link>
-          </li>
-          <li>
-          <Link to="/mywork">Testing Business Ideas</Link>
-          </li>
-          </ul>
+      <Container fluid>
+        <Image src="/space.jpeg" fluid />
+      </Container>
 
-          <h5>Socials:</h5>
-          <ul>
-          <li>
-          <Link to="/mywork">LinkedIn</Link>
-          </li>
-          <li>
-          <Link to="/mywork">GitHub</Link>
-          </li>
-          <li>
-          <Link to="/mywork">Resume</Link>
-          </li>
-          </ul>
 
-      </ul>
+
     </div>
   );}
 
   export default Home;
-
-/*
-  <div>
-  <h1> </h1>
-  <ul>
-    <h5>Examples of my work:</h5>
-      <ul>
-      <li>
-      <Link to="/mywork">Stories</Link>
-      </li>
-      <li>
-      <Link to="/mywork">Epics</Link>
-      </li>
-      <li>
-      <Link to="/mywork">Product Roadmaps</Link>
-      </li>
-      <li>
-      <Link to="/mywork">UI/UX </Link>
-      </li>
-      </ul>
-    
-    
-      <h5>Projects I contributed to:</h5>
-      <ul>
-      <li>
-      <Link to="/mywork">Avalanche Insights</Link>
-      </li>
-      <li>
-      <Link to="/mywork">Emozia</Link>
-      </li>
-      <li>
-      <Link to="/mywork">Together.farm</Link>
-      </li>
-      <li>
-      <Link to="/mywork">MapMyStress</Link>
-      </li>
-      <li>
-      <Link to="/mywork">MapMyWork </Link>
-      </li>
-      <li>
-      <Link to="/mywork">Coronavirus Updates</Link>
-      </li>
-      <li>
-      <Link to="/mywork">NYC Traffic Cams</Link>
-      </li>
-      </ul>
-    
-      <h5>Favorie Books:</h5>
-      <ul>
-      <li>
-      <Link to="/mywork">The Startup Owners Manual</Link>
-      </li>
-      <li>
-      <Link to="/mywork">Testing Business Ideas</Link>
-      </li>
-      </ul>
-
-      <h5>Socials:</h5>
-      <ul>
-      <li>
-      <Link to="/mywork">LinkedIn</Link>
-      </li>
-      <li>
-      <Link to="/mywork">GitHub</Link>
-      </li>
-      <li>
-      <Link to="/mywork">Resume</Link>
-      </li>
-      </ul>
-
-  </ul>
-</div>
-*/
