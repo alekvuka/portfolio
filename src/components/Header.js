9d3dfdacad4507860aaf4a8b634329395819985e
Aleksandar Vukasinovic <alekvuka@gmail.com>
2023-06-05 18:26:14 +0200

    added right references to links

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,10 @@ function Header() {
     <Navbar bg="light" variant="light" expand="lg" className="justify-content-center">
           <Nav bg="light" expand="lg" className="justify-content-center">
             <Nav.Link href="">Home</Nav.Link>
-            <Nav.Link href="#link">Examples of My Work</Nav.Link>
-            <Nav.Link href="#link">Projects I Contribnuted To</Nav.Link>
-            <Nav.Link href="#link">My Favorite Books</Nav.Link>
-            <Nav.Link href="#link">Contact</Nav.Link>
+            <Nav.Link href="mywork">Examples of My Work</Nav.Link>
+            <Nav.Link href="projects">Projects I Contribnuted To</Nav.Link>
+            <Nav.Link href="books">My Favorite Books</Nav.Link>
+            <Nav.Link href="contact">Contact</Nav.Link>
           </Nav>
     </Navbar>
   );
