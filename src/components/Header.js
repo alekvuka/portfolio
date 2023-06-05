941c83b559ea72a60a0e6d4913e2a732eaf33a92
Aleksandar Vukasinovic <alekvuka@gmail.com>
2023-06-05 19:58:26 +0200

    incorporated actiuve links into the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,17 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import './Header.css'; // Import custom CSS file
+import { NavLink, useLocation } from 'react-router-dom';
 
 function Header() {
+  const location = useLocation();
   return (
     <Navbar bg="light" variant="light" expand="lg" className="justify-content-center">
           <Nav bg="light" expand="lg" className="justify-content-center">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="mywork">Examples of My Work</Nav.Link>
-            <Nav.Link href="projects">Projects I Contribnuted To</Nav.Link>
-            <Nav.Link href="books">My Favorite Books</Nav.Link>
+            <Nav.Link  as={NavLink} to="/" exact activeClassName="active"> Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/mywork" activeClassName="active">Examples of My Work</Nav.Link>
+            <Nav.Link as={NavLink} to="/projects" activeClassName="active">Projects I Contribnuted To</Nav.Link>
+            <Nav.Link as={NavLink} to="/books" activeClassName="active">My Favorite Books</Nav.Link>
             <Nav.Link href="contact">Contact</Nav.Link>
           </Nav>
     </Navbar>
