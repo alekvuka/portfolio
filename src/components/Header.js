b7ad71306fbce67a97112341b38ebf4af60b7f1e
Aleksandar Vukasinovic <alekvuka@gmail.com>
2023-06-05 18:19:51 +0200

    added Header.css that stylized the header links

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,20 +2,19 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Header.css'; // Import custom CSS file
 
 function Header() {
   return (
-    
-    <Navbar bg="light" expand="lg" className="justify-content-center">
+    <Navbar bg="light" variant="light" expand="lg" className="justify-content-center">
           <Nav bg="light" expand="lg" className="justify-content-center">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="">Home</Nav.Link>
             <Nav.Link href="#link">Examples of My Work</Nav.Link>
             <Nav.Link href="#link">Projects I Contribnuted To</Nav.Link>
             <Nav.Link href="#link">My Favorite Books</Nav.Link>
             <Nav.Link href="#link">Contact</Nav.Link>
           </Nav>
     </Navbar>
-    
   );
 }
 
