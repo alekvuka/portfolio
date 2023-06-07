d6788282826c1f05d8b33499a1ed7d72a7d71aa5
Aleksandar Vukasinovic <alekvuka@gmail.com>
2023-06-07 18:59:42 +0200

    added cards component to books page

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,9 +1,53 @@
 // Books component
+import React from 'react';
+import { Container, Image, Row, Col } from 'react-bootstrap';
+import Header from '../components/Header';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+
+
 const Books = () => {
     return (
       <div>
-        <h1>Favorite Books</h1>
-        {/* Add your content for Favorite Books page */}
+        <Header />
+        <Container
+         className="d-flex flex-column align-items-center justify-content-center"
+         style={{ paddingTop: '15px' }}>
+        <Card style={{ width: '18rem',  marginTop: '45px'  }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text> 
+            <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem',  marginTop: '45px'  }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text> 
+            <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+        <Card style={{ width: '18rem',  marginTop: '45px', marginBottom: '45px'  }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text> 
+            <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+        </Card>
+         </Container>
       </div>
     );
   };
